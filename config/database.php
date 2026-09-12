5a7bb444bbf3ece3b543e61b5bf01fd6e17e747f
ndeyefatoundiour <ndeyefatoundiour530@gmail.com>
2026-09-12 12:56:42 +0000

    Mise à jour de la configuration Docker database

--- config/database.php
+++ config/database.php
@@ -8,12 +8,12 @@ $capsule = new Capsule();
 
 try {
     $capsule->addConnection([
-        'driver' => $_ENV['DB_DRIVER'] ?? 'mysql',
-        'host' => $_ENV['DB_HOST'] ?? 'db',
-        'port' => (int) ($_ENV['DB_PORT'] ?? 3306),
-        'database' => $_ENV['DB_DATABASE'] ?? 'reservation_salles',
-        'username' => $_ENV['DB_USERNAME'] ?? 'root',
-        'password' => $_ENV['DB_PASSWORD'] ?? 'root_password',
+        'driver' => $_ENV['DB_DRIVER'] ,
+        'host' => $_ENV['DB_HOST'] ,
+        'port' => (int) ($_ENV['DB_PORT']),
+        'database' => $_ENV['DB_DATABASE'] ,
+        'username' => $_ENV['DB_USERNAME'] ,
+        'password' => $_ENV['DB_PASSWORD'] ,
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
         'prefix' => '',
